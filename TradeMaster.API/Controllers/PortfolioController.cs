6ecad9a12f049d095532d4adef9a1a3e9fffce19
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-25 23:19:19 +0530

    Added Portfolio COntroller With Authorization

---
+++ TradeMaster.API/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TradeMaster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+
+        public IActionResult GetPortfolio()
+        {
+            return Ok("Welcome To TradeMaster Portfolio");
+        }
+    }
+}
