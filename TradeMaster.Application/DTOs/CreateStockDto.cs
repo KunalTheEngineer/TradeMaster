45e1650566fcd686e174d43ac25be97bd2b0ed19
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-26 09:53:22 +0530

    Completed Stock CRUD Module

---
+++ TradeMaster.Application/DTOs/CreateStockDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TradeMaster.Application.DTOs
+{
+    public class CreateStockDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public Decimal CurrentPrice { get; set; }
+    }
+}
