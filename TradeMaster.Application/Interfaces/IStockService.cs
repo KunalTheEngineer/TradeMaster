faa7a2478f574bf4c8029fd13731ce7607f66e82
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-26 10:04:47 +0530

    Completed Stock CRUD Module

---
+++ TradeMaster.Application/Interfaces/IStockService.cs
@@ -0,0 +1,18 @@
+﻿
+using TradeMaster.Application.DTOs;
+
+namespace TradeMaster.Application.Interfaces
+{
+    public interface IStockService
+    {
+        Task<string> AddStockAsync(CreateStockDto request);
+
+        Task<List<StockResponseDto>> GetAllStockAsync();
+
+        Task<StockResponseDto?> GetStockByIdAsync(int stockId);
+
+        Task<string> UpdateStockAsync(int stockId, UpdateStockDto request);
+
+        Task<string> DeleteStockAsync(int stockId);
+    }
+}
