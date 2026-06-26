d6a6799a27fccf3d260cac69b0cb45840943651d
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-26 10:05:24 +0530

    Completed Stock CRUD Module

---
+++ TradeMaster.Core/Interfaces/IStockRepository.cs
@@ -0,0 +1,17 @@
+﻿using TradeMaster.Core.Entities;
+
+namespace TradeMaster.Core.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task AddStockAsync(Stock stock);
+
+        Task<List<Stock>> GetAllStocksAsync();  
+
+        Task<Stock?> GetStockByIdAsync(int stockId);
+
+        Task UpdateStockAsync(Stock stock);
+
+        Task DeleteStockAsync(Stock stock);
+    }
+}
