d7bb6407fd642680ba85c33230f4957186b6e2f0
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-28 23:22:04 +0530

    Added Portfolio Summary API

---
+++ TradeMaster.Application/DTOs/PortfolioSummaryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeMaster.Application.DTOs
+{
+    public class PortfolioSummaryDto
+    {
+        public int TotalHoldings { get; set; }
+
+        public int TotalQuantity { get; set; }
+
+        public decimal TotalInvestment { get; set; }
+
+       // public decimal CurrentPortfolioValue { get; set; }
+    }
+}
