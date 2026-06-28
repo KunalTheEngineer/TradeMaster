4300aa24e24b42aed03e6c1f36af463e3da75d8e
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-28 23:22:41 +0530

    Added Portfolio Summary API

---
+++ TradeMaster.Application/Interfaces/IPortfolioService.cs
@@ -0,0 +1,9 @@
+﻿using TradeMaster.Application.DTOs;
+
+namespace TradeMaster.Application.Interfaces
+{
+    public interface IPortfolioService
+    {
+        Task<PortfolioSummaryDto> GetPortfolioSummaryAsync(int userId);
+    }
+}
