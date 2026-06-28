1c50161afa15833f903fd3e55b6bacb005cc253e
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-28 23:24:45 +0530

    Added Portfolio Summary API

---
+++ TradeMaster.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TradeMaster.Core.Entities;
+using TradeMaster.Core.Interfaces;
+using TradeMaster.Infrastructure.Data;
+
+namespace TradeMaster.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly TradeMasterDbContext _context;
+
+        public TransactionRepository(TradeMasterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTransactionAsync(Transaction transaction)
+        {
+            await _context.Transactions.AddAsync(transaction);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Transaction>> GetAllTransactionAsync()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+    }
+}
