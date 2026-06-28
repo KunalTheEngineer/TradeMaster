f4b7465f233698219f4ce00f7d82dcdc14a127ef
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-28 23:23:53 +0530

    Added Portfolio Summary API

---
+++ TradeMaster.Core/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using TradeMaster.Core.Entities;
+
+namespace TradeMaster.Core.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task AddTransactionAsync(Transaction transaction);
+
+        Task<List<Transaction>> GetAllTransactionAsync();
+    }
+}
