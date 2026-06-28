192bbb29f9fe8dd9bbcac451c4a21f898c4173d5
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-28 23:25:21 +0530

    Added Portfolio Summary API

---
+++ TradeMaster.Infrastructure/Services/PortfolioService.cs
@@ -0,0 +1,29 @@
+﻿using TradeMaster.Application.DTOs;
+using TradeMaster.Application.Interfaces;
+using TradeMaster.Core.Interfaces;
+
+namespace TradeMaster.Infrastructure.Services
+{
+    public class PortfolioService : IPortfolioService
+    {
+        //HOLDINGS table contain quntity and avgprice so we taken this below
+        private readonly IHoldingRepository _holdingRepository;
+
+        public PortfolioService(IHoldingRepository holdingRepository)
+        {
+            _holdingRepository = holdingRepository;
+        }
+
+        public async Task<PortfolioSummaryDto> GetPortfolioSummaryAsync(int userId)
+        {
+            var holdings = await _holdingRepository.GetHoldingsByUserIdAsync(userId);
+
+            return new PortfolioSummaryDto
+            {
+                TotalHoldings = holdings.Count,
+                TotalQuantity = holdings.Sum(h => h.Qunatity),
+                TotalInvestment = holdings.Sum(h => h.Qunatity * h.AveragePrice)
+            };
+        }
+    }
+}
