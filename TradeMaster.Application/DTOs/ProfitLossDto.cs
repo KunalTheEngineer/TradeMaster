f078010d1bdf2da2779783d25cc42a5fb20a2951
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-29 23:57:40 +0530

    implement Portfolio Profit and Loss API

---
+++ TradeMaster.Application/DTOs/ProfitLossDto.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TradeMaster.Application.DTOs
+{
+    public class ProfitLossDto
+    {
+        public string StockName { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal AveragePrice { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public decimal TotalInvestment { get; set; }
+
+        public decimal CurrentValue { get; set; }
+
+        public decimal ProfitLoss { get; set; }
+    }
+}
