215e7af184e533d736539f0edbde4588bcaf7bee
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-29 23:58:26 +0530

    Implement Portfolio Proft and Loss API

--- TradeMaster.API/Controllers/PortfolioController.cs
+++ TradeMaster.API/Controllers/PortfolioController.cs
@@ -1,19 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using TradeMaster.Application.Interfaces;
 
 namespace TradeMaster.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PortfolioController : ControllerBase
     {
-        [Authorize]
-        [HttpGet]
+        private readonly IPortfolioService _portfolioService;
 
-        public IActionResult GetPortfolio()
+        public PortfolioController(IPortfolioService portfolioService)
         {
-            return Ok("Welcome To TradeMaster Portfolio");
+            _portfolioService = portfolioService;
+        }
+
+        [HttpGet("summary/{userId}")]
+        public async Task<IActionResult> GetPortfolioSummary(int userId)
+        {
+            var summary = await _portfolioService.GetPortfolioSummaryAsync(userId);
+
+            return Ok(summary);
+        }
+
+        [HttpGet("profit-loss/{userId}")]
+        public async Task<IActionResult> GetProfitLoss(int userId)
+        {
+            var result = await _portfolioService.GetProfitLossAsync(userId);
+
+            return Ok(result);
         }
     }
 }
