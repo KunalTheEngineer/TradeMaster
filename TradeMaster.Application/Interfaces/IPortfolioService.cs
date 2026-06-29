f96c0b732f39cfcf90d578e1963b48e14a28a7a0
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-06-29 23:59:14 +0530

    Implement Portfolio Profit and Loss API

--- TradeMaster.Application/Interfaces/IPortfolioService.cs
+++ TradeMaster.Application/Interfaces/IPortfolioService.cs
@@ -1,9 +1,12 @@
 ﻿using TradeMaster.Application.DTOs;
+using System.Collections.Generic;
 
 namespace TradeMaster.Application.Interfaces
 {
     public interface IPortfolioService
     {
         Task<PortfolioSummaryDto> GetPortfolioSummaryAsync(int userId);
+
+        Task<List<ProfitLossDto>> GetProfitLossAsync(int userId);
     }
 }
