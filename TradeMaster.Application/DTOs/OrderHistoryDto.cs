51d78b8dbffc98769b1f0bf5b721ef215af4f2c3
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-01 14:04:07 +0530

    Completed WatchList module and Portfolio Mangement features

---
+++ TradeMaster.Application/DTOs/OrderHistoryDto.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TradeMaster.Application.DTOs
+{
+    public class OrderHistoryDto
+    {
+        public int OrderId { get; set; }
+
+        public string StockName { get; set; } = string.Empty;
+
+        public string OrderType { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string OrderStatus { get; set; } = string.Empty;
+
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ TradeMaster.Application/DTOs/WatchListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeMaster.Application.DTOs
+{
+    public class WatchListDto
+    {
+        public int WatchlistId { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public decimal CurrentPrice { get; set; }
+    }
+}
