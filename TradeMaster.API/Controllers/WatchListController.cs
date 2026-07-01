633eba4303a1ba318ad08db565d1f8cafea0de00
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-01 14:02:58 +0530

    Completed WatchList module and Portfolio Management Features

---
+++ TradeMaster.API/Controllers/WatchListController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TradeMaster.Application.DTOs;
+using TradeMaster.Application.Interfaces;
+using TradeMaster.Infrastructure.Services;
+
+namespace TradeMaster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WatchListController : ControllerBase
+    {
+        private readonly IWatchListService _watchListService;
+        public WatchListController(IWatchListService watchListService)
+        {
+            _watchListService = watchListService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToWatchlist(AddWatchListDto dto)
+        {
+            var result = await _watchListService.AddToWatchlistAsync(dto);
+            return Ok(result);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUserWatchlist(int userId)
+        {
+            var result = await _watchListService.GetUserWatchlistAsync(userId);
+            return Ok(result);
+        }
+
+        [HttpDelete("{userId}/{stockId}")]
+        public async Task<IActionResult> RemoveFromWatchlist(int userId, int stockId)
+        {
+            var result = await _watchListService.RemoveFromWatchlistAsync(userId, stockId);
+            return Ok(result);
+        }
+    }
+}
---
+++ TradeMaster.Application/DTOs/AddWatchListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeMaster.Application.DTOs
+{
+    public class AddWatchListDto
+    {
+        public int UserId { get; set; }
+
+        public int StockId { get; set; }
+    }
+}
