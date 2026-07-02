9731c16254773c4cee9fb821551f062f11800b6c
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-02 14:08:20 +0530

    Completed WatchList module and Portfolio Mangement Features

---
+++ TradeMaster.Infrastructure/Repositories/WatchListRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradeMaster.Core.Entities;
+using TradeMaster.Core.Interfaces;
+using TradeMaster.Infrastructure.Data;
+
+namespace TradeMaster.Infrastructure.Repositories
+{
+    public class WatchListRepository :IWatchListRepository
+    {
+        private readonly TradeMasterDbContext _context;
+
+        public WatchListRepository(TradeMasterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddToWatchlistAsync(Watchlist watchlist)
+        {
+            await _context.Watchlists.AddAsync(watchlist);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Watchlist>> GetUserWatchlistAsync(int userId)
+        {
+            return await _context.Watchlists
+                .Where(w => w.UserId == userId)
+                .Include(w => w.Stock)
+                .ToListAsync();
+        }
+
+        public async Task<Watchlist?> GetWatchlistItemAsync(int userId, int stockId)
+        {
+            return await _context.Watchlists
+                .FirstOrDefaultAsync(w => w.UserId == userId && w.StockId == stockId);
+        }
+
+        public async Task RemoveFromWatchlistAsync(Watchlist watchlist)
+        {
+            _context.Watchlists.Remove(watchlist);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TradeMaster.Infrastructure/Services/WatchListService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeMaster.Application.DTOs;
+using TradeMaster.Application.Interfaces;
+using TradeMaster.Core.Entities;
+using TradeMaster.Core.Interfaces;
+using TradeMaster.Infrastructure.Repositories;
+
+namespace TradeMaster.Infrastructure.Services
+{
+    public class WatchListService : IWatchListService
+    {
+        private readonly IWatchListRepository _watchlistRepository;
+
+        public WatchListService(IWatchListRepository repository)
+        {
+            _watchlistRepository = repository;
+        }
+
+        public async Task<string> AddToWatchlistAsync(AddWatchListDto dto)
+        {
+            var existing = await _watchlistRepository
+                .GetWatchlistItemAsync(dto.UserId, dto.StockId);
+
+            if (existing != null)
+                return "Stock already exists in watchlist.";
+
+            var watchlist = new Watchlist
+            {
+                UserId = dto.UserId,
+                StockId = dto.StockId
+            };
+
+            await _watchlistRepository.AddToWatchlistAsync(watchlist);
+
+            return "Stock added to watchlist successfully.";
+        }
+
+        public async Task<List<WatchListDto>> GetUserWatchlistAsync(int userId)
+        {
+            var watchlist = await _watchlistRepository
+                .GetUserWatchlistAsync(userId);
+
+            return watchlist.Select(w => new WatchListDto
+            {
+                WatchlistId = w.WatchlistId,
+                Symbol = w.Stock.Symbol,
+                CompanyName = w.Stock.CompanyName,
+                CurrentPrice = w.Stock.CurrentPrice
+            }).ToList();
+        }
+
+        public async Task<string> RemoveFromWatchlistAsync(int userId, int stockId)
+        {
+            var watchlist = await _watchlistRepository
+                .GetWatchlistItemAsync(userId, stockId);
+
+            if (watchlist == null)
+                return "Stock not found in watchlist.";
+
+            await _watchlistRepository.RemoveFromWatchlistAsync(watchlist);
+
+            return "Stock removed from watchlist successfully.";
+        }
+    }
+}
