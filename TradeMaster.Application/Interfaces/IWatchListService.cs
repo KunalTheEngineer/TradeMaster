fc9dfe40d7009b5e552f73527aad91200caad9fd
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-02 14:05:41 +0530

    Completed WatchList module and Portfolio Management Services

---
+++ TradeMaster.Application/Interfaces/IWatchListService.cs
@@ -0,0 +1,13 @@
+﻿using TradeMaster.Application.DTOs;
+
+namespace TradeMaster.Application.Interfaces
+{
+    public interface IWatchListService
+    {
+        Task<string> AddToWatchlistAsync(AddWatchListDto dto);
+
+        Task<List<WatchListDto>> GetUserWatchlistAsync(int userId);
+
+        Task<string> RemoveFromWatchlistAsync(int userId, int stockId);
+    }
+}
---
+++ TradeMaster.Core/Interfaces/IWatchListRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeMaster.Core.Entities;
+
+namespace TradeMaster.Core.Interfaces
+{
+    public interface IWatchListRepository
+    {
+        Task AddToWatchlistAsync(Watchlist watchlist);
+
+        Task<List<Watchlist>> GetUserWatchlistAsync(int userId);
+
+        Task<Watchlist?> GetWatchlistItemAsync(int userId, int stockId);
+
+        Task RemoveFromWatchlistAsync(Watchlist watchlist);
+    }
+}
