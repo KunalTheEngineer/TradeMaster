a73196053c80f60e9e158b78ca37256a9b673f0b
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-03 15:54:57 +0530

    Implemented Global Exception Handling

---
+++ TradeMaster.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using TradeMaster.Application.Responses;
+
+namespace TradeMaster.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.ContentType = "application/json";
+
+                context.Response.StatusCode = ex switch
+                {
+                    ArgumentException => StatusCodes.Status400BadRequest,
+
+                    KeyNotFoundException => StatusCodes.Status404NotFound,
+
+                    UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                var response = new ApiErrorResponse
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = ex.Message
+                };
+
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(response));
+            }
+        }
+
+    }
+}
