bc49396d5154d600fc315f363a6fab1a496a49c7
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-03 15:56:18 +0530

    Implement structured business logging with Serilog

--- TradeMaster.Infrastructure/Services/WatchListService.cs
+++ TradeMaster.Infrastructure/Services/WatchListService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,20 +15,43 @@ namespace TradeMaster.Infrastructure.Services
     public class WatchListService : IWatchListService
     {
         private readonly IWatchListRepository _watchlistRepository;
+        private readonly ILogger<WatchListService> _logger;
+        private readonly IStockRepository _stockRepository;
 
-        public WatchListService(IWatchListRepository repository)
+        public WatchListService(IWatchListRepository repository, IStockRepository stockRepository, ILogger<WatchListService> logger)
         {
             _watchlistRepository = repository;
+            _stockRepository = stockRepository;
+            _logger = logger;
         }
 
         public async Task<string> AddToWatchlistAsync(AddWatchListDto dto)
         {
+            var stock = await _stockRepository.GetStockByIdAsync(dto.StockId);
+
+            if (stock == null)
+            {
+                _logger.LogWarning(
+                    "WATCHLIST | User: {UserId} | Stock {StockId} not found.",
+                    dto.UserId,
+                    dto.StockId);
+
+                throw new KeyNotFoundException("Stock not found.");
+            }
+
             var existing = await _watchlistRepository
                 .GetWatchlistItemAsync(dto.UserId, dto.StockId);
 
             if (existing != null)
-                return "Stock already exists in watchlist.";
+            {
+                _logger.LogWarning(
+                "WATCHLIST | User: {UserId} | Stock: {Symbol} already exists in watchlist.",
+                dto.UserId,
+                 stock.Symbol);
 
+                throw new ArgumentException("Stock already exists in watchlist.");
+            }
+               
             var watchlist = new Watchlist
             {
                 UserId = dto.UserId,
@@ -35,6 +59,11 @@ namespace TradeMaster.Infrastructure.Services
             };
 
             await _watchlistRepository.AddToWatchlistAsync(watchlist);
+            
+            _logger.LogInformation(
+                "WATCHLIST | User: {UserId} | Added Stock: {Symbol}",
+                dto.UserId,
+                stock.Symbol);
 
             return "Stock added to watchlist successfully.";
         }
@@ -55,13 +84,36 @@ namespace TradeMaster.Infrastructure.Services
 
         public async Task<string> RemoveFromWatchlistAsync(int userId, int stockId)
         {
+            var stock = await _stockRepository.GetStockByIdAsync(stockId);
+
+            if (stock == null)
+            {
+                _logger.LogWarning(
+                    "WATCHLIST | User: {UserId} | Stock {StockId} not found.",
+                    userId,
+                    stockId);
+
+                throw new KeyNotFoundException("Stock not found.");
+            }
+
             var watchlist = await _watchlistRepository
                 .GetWatchlistItemAsync(userId, stockId);
 
             if (watchlist == null)
-                return "Stock not found in watchlist.";
+            {
+                _logger.LogWarning(
+                  "WATCHLIST | User: {UserId} | Stock: {Symbol} not found in watchlist.",
+                   userId,
+                    stock.Symbol);
 
+                throw new KeyNotFoundException("Stock not found in watchlist.");
+            }
+            
             await _watchlistRepository.RemoveFromWatchlistAsync(watchlist);
+            _logger.LogInformation(
+            "WATCHLIST | User: {UserId} | Removed Stock: {Symbol}",
+            userId,
+            stock.Symbol);
 
             return "Stock removed from watchlist successfully.";
         }
