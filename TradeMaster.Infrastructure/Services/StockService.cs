56c1a3d97198c191286fd070863a75479f4ea03f
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-03 15:57:37 +0530

    Implement structured business logging with Serilog

--- TradeMaster.Infrastructure/Services/StockService.cs
+++ TradeMaster.Infrastructure/Services/StockService.cs
@@ -2,16 +2,18 @@
 using TradeMaster.Core.Entities;
 using TradeMaster.Core.Interfaces;
 using TradeMaster.Application.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace TradeMaster.Infrastructure.Services
 {
     public class StockService : IStockService
     {
         private readonly IStockRepository _stockRepository;
-
-        public StockService(IStockRepository stockRepository)
+        private readonly ILogger<StockService> _logger;
+        public StockService(IStockRepository stockRepository, ILogger<StockService> logger)
         {
             _stockRepository = stockRepository;
+            _logger = logger;
         }
 
         public async Task<string> AddStockAsync(CreateStockDto request)
@@ -24,6 +26,12 @@ namespace TradeMaster.Infrastructure.Services
             };
 
             await _stockRepository.AddStockAsync(stock);
+
+            _logger.LogInformation(
+              "ADMIN ACTION | Added Stock | Symbol: {Symbol} | Company: {CompanyName} | Price: {Price}",
+             stock.Symbol,
+             stock.CompanyName,
+             stock.CurrentPrice);
 
             return "Stock Added Successfully";
         }
@@ -45,7 +53,11 @@ namespace TradeMaster.Infrastructure.Services
 
             if(stock == null)
             {
-                return null;
+                _logger.LogWarning(
+                 "ADMIN ACTION | Update failed. Stock {StockId} not found.",
+                     stockId);
+
+                throw new KeyNotFoundException("Stock not found.");
             }
 
             return new StockResponseDto
@@ -63,7 +75,14 @@ namespace TradeMaster.Infrastructure.Services
 
             if (stock == null)
             {
-                return "Stock Not Found !";
+                if (stock == null)
+                {
+                    _logger.LogWarning(
+                        "ADMIN ACTION | Update failed. Stock {StockId} not found.",
+                        stockId);
+
+                    throw new KeyNotFoundException("Stock not found.");
+                }
             }
             
             stock.Symbol = request.Symbol;
@@ -71,6 +90,12 @@ namespace TradeMaster.Infrastructure.Services
             stock.CurrentPrice = request.CurrentPrice;
 
             await _stockRepository.UpdateStockAsync(stock);
+
+            _logger.LogInformation(
+             "ADMIN ACTION | Updated Stock | Symbol: {Symbol} | Company: {CompanyName} | New Price: {Price}",
+             stock.Symbol,
+             stock.CompanyName,
+             stock.CurrentPrice);
 
             return "Stock Updated Successfully !";
             
@@ -82,10 +107,17 @@ namespace TradeMaster.Infrastructure.Services
 
             if(stock == null)
             {
-                return "Stock Not Found !";
+                _logger.LogWarning(
+                  "ADMIN ACTION | Delete failed. Stock {StockId} not found.",
+                    stockId);
+
+                throw new KeyNotFoundException("Stock not found.");
             }
 
             await _stockRepository.DeleteStockAsync(stock);
+            _logger.LogInformation(
+              "ADMIN ACTION | Deleted Stock | Symbol: {Symbol}",
+             stock.Symbol);
 
             return "Stock Deleted Successfully !";
         }
