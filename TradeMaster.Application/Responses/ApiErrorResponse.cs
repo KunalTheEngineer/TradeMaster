b0229d103bad458467af3430a7186ae98bba1a25
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-07-04 17:12:41 +0530

    Implement Pagination and its features

---
+++ TradeMaster.Application/Responses/ApiErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeMaster.Application.Responses
+{
+    public class ApiErrorResponse
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
